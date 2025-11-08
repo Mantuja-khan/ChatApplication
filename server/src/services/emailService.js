c8801da73181ebbe8a3e1681fd72e08f2339dafd
Mantuja-khan <133602638+Mantuja-khan@users.noreply.github.com>
2025-11-08 11:45:46 +0530

    emailService.js
    Updated email service to use hardcoded Gmail credentials.

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -76,8 +76,8 @@ export const sendOTPEmail = async (email) => {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD
+       user: "mantujak@gmail.com",
+        pass: "axyo delf gmbm zobb"
       }
     });
 
